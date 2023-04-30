64e4ea616891c0d59faafde99dfe86a1b70448a6
laoweng <229595412@qq.com>
2023-04-30 22:42:16 +0800

    init

---
+++ src/test/java/com/laowengs/ms/ops/admin/MsOpsAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laowengs.ms.ops.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOpsAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
